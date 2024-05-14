f88e9399048526607d0155547da19991a6c224a1
saugat23 <saugatbhandari8@gmail.com>
2024-05-14 18:18:27 +0545

    demonode code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -106,7 +106,7 @@
 
                 <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
                     <div class="text-center text-sm sm:text-left">
-                        &nbsp;
+                        &nbsp; here added some lines
                     </div>
 
                     <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
