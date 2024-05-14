e4e32a38ac0ad285d1f5fcb736178ed98ed160a4
saugat23 <saugatbhandari8@gmail.com>
2024-05-14 18:36:12 +0545

    commerce

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -109,6 +109,54 @@
                         &nbsp; here added some lines some more lines here we go!!!
                     </div>
 
+                     <div class="col-6 col-md-4 col-lg-3">
+                <div class="card mb-4 bg-warning border-0">
+                    <img class="card-img-top" src="holder.js/100x180/" alt="">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <i class="icofont icofont-slack"></i>
+                            Categories
+                        </h4>
+                        <h5 class="card-text"></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-4 col-lg-3">
+                <div class="card mb-4 bg-info border-0">
+                    <img class="card-img-top" src="holder.js/100x180/" alt="">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <i class="icofont icofont-food-basket"></i>
+                            Products
+                        </h4>
+                        <h5 class="card-text"></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-4 col-lg-3">
+                <div class="card mb-4 bg-light border-0">
+                    <img class="card-img-top" src="holder.js/100x180/" alt="">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <i class="icofont icofont-users"></i>
+                            Users
+                        </h4>
+                        <h5 class="card-text"></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-4 col-lg-3">
+                <div class="card mb-4 bg-success bg-opacity-25 border-0">
+                    <img class="card-img-top" src="holder.js/100x180/" alt="">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <i class="icofont icofont-basket"></i>
+                            Orders
+                        </h4>
+                        <h5 class="card-text"></h5>
+                    </div>
+                </div>
+            </div>
                     <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
